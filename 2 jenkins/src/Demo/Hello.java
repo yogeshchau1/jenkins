157c24acfb291999b0911d77dbf2f731271fc3e8
Hp <Hp@RIYA>
2021-12-27 10:16:54 +0530

    By yog

--- 2 jenkins/src/Demo/Hello.java
+++ 2 jenkins/src/Demo/Hello.java
@@ -2,11 +2,9 @@ package Demo;
 
 public class Hello {
 
-	
 	public static void main(String[] args) {
-		
-		
-		for(int i=0; i>10;i++)
+		 
+		for(int i=0;i<9;i++)
 		{
 			System.out.println("HELLO");
 		}
